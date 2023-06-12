5779ca76165d90d3d0d25bb138853edc67442e4e
selene castillo <secast@live.com>
2023-06-12 15:00:00 -0700

    added animal list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Animal from './components/Animal';
+import AnimalList from './components/AnimalList';
 
 function App() {
   return (
     <section>
       <h1>Sapp Animal Sanc</h1>
       <h2>Animal Listings</h2>
+      <AnimalList></AnimalList>
       <Animal></Animal>
       <Animal></Animal>
       <Animal></Animal>
