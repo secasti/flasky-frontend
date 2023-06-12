1b7e1651280f24303a349e78259e3321acdbf2fe
selene castillo <secast@live.com>
2023-06-12 14:55:36 -0700

    first commit)

---
+++ src/components/Animal.js
@@ -0,0 +1,13 @@
+import './Animal.css';
+
+const Animal = () => {
+    return(<section className='Animal'>
+        <h3>Animal name: JuniBeary</h3>
+        <p>Species: poopies</p>
+        <img
+        src="https://www.instagram.com/p/Cn5ig0wP5bp/?utm_source=ig_embed&amp;utm_campaign=loading"
+        alt="pic of bbs"
+      />    </section>);
+};
+
+export default Animal
